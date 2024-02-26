be1da7c6f5603b084d4c2de3b6090237d9284026
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2024-02-26 01:18:50 -0500

    Create termux-uninstall.sh

---
+++ DEDMAP/termux-uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm ~/../usr/bin/dedmap
+rm -rf ~/../usr/share/dedmap/
+echo -e "\n.......................DEDMAP UNINSTALLED............................"
+echo -e "\nIt's sad to see you leave :(\n"
